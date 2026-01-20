1f1afde80bcc47ca2f281ed76f14a7a3b824ed17
Maksim Zadorozhnyi <m.zadorozhnyi@projectfirst.tv>
2026-01-20 12:08:41 +0300

    add UserCardControllerTest

---
+++ src/test/java/com/example/bankcards/WebConfig.java
@@ -0,0 +1,9 @@
+package com.example.bankcards;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig {
+}
